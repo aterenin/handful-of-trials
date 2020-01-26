116add16a30ec8360db7d74d6446a185cece759f
aterenin <aterenin@users.noreply.github.com>
2020-01-26 13:41:50 +0000

    Add CartPole modifications: stop on hitting wall.

--- dmbrl/env/cartpolebullet.py
+++ dmbrl/env/cartpolebullet.py
@@ -9,17 +9,17 @@ import numpy as np
 import pybullet
 from pybullet_envs.scene_abstract import SingleRobotEmptyScene
 from pybullet_envs.env_bases import MJCFBaseBulletEnv
-from pybullet_envs.robot_pendula import InvertedPendulumSwingup
+from pybullet_envs.robot_bases import MJCFBasedRobot
 
 class CartpoleBulletEnv(MJCFBaseBulletEnv):
 
   def __init__(self):
-    self.robot = InvertedPendulumSwingup()
+    self.robot = InvertedPendulumModified()
     MJCFBaseBulletEnv.__init__(self, self.robot)
     self.stateId = -1
 
   def create_single_player_scene(self, bullet_client):
-    return SingleRobotEmptyScene(bullet_client, gravity=9.8, timestep=0.0165, frame_skip=1)
+    return SingleRobotEmptyScene(bullet_client, gravity=9.8, timestep=0.05, frame_skip=1)
 
   def reset(self):
     if (self.stateId >= 0):
@@ -38,7 +38,7 @@ class CartpoleBulletEnv(MJCFBaseBulletEnv):
     vel_penalty = 0
     if self.robot.swingup:
       reward = np.cos(self.robot.theta)
-      done = False
+      done = np.abs(state[0]) > 4.99
     else:
       reward = 1.0
       done = np.abs(self.robot.theta) > .2
@@ -53,3 +53,52 @@ class CartpoleBulletEnv(MJCFBaseBulletEnv):
     if mode != "rgb_array":
       return np.array([])
     return super().render(mode,close).astype('uint8')
+
+class InvertedPendulumModified(MJCFBasedRobot):
+	swingup = True
+	def __init__(self):
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+		MJCFBasedRobot.__init__(self, '%s/assets/inverted_pendulum.xml' % dir_path, 'cart', action_dim=1, obs_dim=4)
+
+	def robot_specific_reset(self, bullet_client):
+		self._p = bullet_client
+		self.pole = self.parts["pole"]
+		self.slider = self.jdict["slider"]
+		self.j1 = self.jdict["hinge"]
+		u = self.np_random.uniform(low=-.1, high=.1)
+		self.j1.reset_current_position( u if not self.swingup else 3.1415+u , 0)
+		self.j1.set_motor_torque(0)
+
+	def apply_action(self, a):
+		assert( np.isfinite(a).all() )
+		if not np.isfinite(a).all():
+			print("a is inf")
+			a[0] = 0
+		self.slider.set_motor_torque(  100*float(np.clip(a[0], -1, +1)) )
+
+	def calc_state(self):
+		self.theta, theta_dot = self.j1.current_position()
+		x, vx = self.slider.current_position()
+		assert( np.isfinite(x) )
+
+		if not np.isfinite(x):
+			print("x is inf")
+			x = 0
+
+		if not np.isfinite(vx):
+			print("vx is inf")
+			vx = 0
+
+		if not np.isfinite(self.theta):
+			print("theta is inf")
+			self.theta = 0
+
+		if not np.isfinite(theta_dot):
+			print("theta_dot is inf")
+			theta_dot = 0
+
+		return np.array([
+			x, vx,
+			# np.cos(self.theta), np.sin(self.theta),
+			theta_dot, self.theta
+			])
